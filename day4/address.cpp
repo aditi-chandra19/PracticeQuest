201037718b30ccc57e9f37788aa8e1d57dda2953
Aditi Chandra <aditichandra126@gmail.com>
2026-05-05 13:54:41 +0530

    address using pointers

--- day4/address.cpp
+++ day4/address.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 int main(){
     int a=5;
-    cout<<&a;
+    int *ptr = &a;
+    int **p2=&ptr;
+    // cout<<&a<<endl;
+    // cout<<*ptr<<endl;
+    // cout<<ptr<<endl;
+    cout<<a<<" "<<ptr<<" "<<p2<<" "<<&p2<<endl;
+    cout<< *ptr<<" "<<*p2<<" "<<**p2<<endl;
 }
