9f9fba819e1e64a2e7d7638b1fed22a5d3f0d176
Aditi Chandra <aditichandra126@gmail.com>
2026-05-07 15:01:30 +0530

    shapes

--- day6/inherit.cpp
+++ day6/inherit.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
-class Animal{
+class Animal{ //abstract class
     public:
     void eat() {
         cout<<"\neating\n";
     }
+   virtual void speak()=0; //virtual funciton pure
     
 };
 class dog : public Animal{//dog inherited animal
@@ -20,11 +21,19 @@ class cat : public Animal{
         cout<<"scratches alot";
     }
 };
+class cow : public Animal{
+    public:
+    void speak(){cout<<"meow\n";}
+};
 int main() {
-    dog d;
-    cat c;
-    d.eat();
-    d.speak();
-    c.eat();
-    c.scratch();
+    
+   dog d;
+//    Animal m; //object of abstract class type "Animal" is not allowed
+cow c;
+d.eat();
+d.speak();
+c.eat();
+c.speak();
+
+ 
 }
---
+++ day6/shapes.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+   public:
+    Shape() {
+        cout << "constructor created" << endl;
+    }
+
+    virtual float area() = 0;//virtual function coz we are using abstract class
+    virtual float perimeter() = 0;
+
+    virtual ~Shape() {
+        cout << "constructor destroyed" << endl;
+    }
+};
+
+class Circle : public Shape {
+    float radius;
+
+   public:
+    Circle(float r) : radius(r) {}
+
+    float area() {
+        return 3.14 * radius * radius;
+    }
+
+    float perimeter() {
+        return 2 * 3.14 * radius;
+    }
+};
+
+class Square : public Shape {
+    float side;
+
+   public:
+    Square(float s) : side(s) {}
+
+    float area() {
+        return side * side;
+    }
+
+    float perimeter() {
+        return 4 * side;
+    }
+};
+
+class Rectangle : public Shape {
+    float length;
+    float breadth;
+
+   public:
+    Rectangle(float l, float b) : length(l), breadth(b) {}
+
+    float area() {
+        return length * breadth;
+    }
+
+    float perimeter() {
+        return 2 * (length + breadth);
+    }
+};
+
+int main() {
+    int choice;
+
+    cout << "1. Circle" << endl;
+    cout << "2. Square" << endl;
+    cout << "3. Rectangle" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1: {
+            float radius;
+            cout << "Enter radius: ";
+            cin >> radius;
+
+            Circle c(radius);
+            Shape& shape = c;
+            cout << "Area = " << shape.area() << endl;
+            cout << "Perimeter = " << shape.perimeter() << endl;
+            break;
+        }
+
+        case 2: {
+            float side;
+            cout << "Enter side: ";
+            cin >> side;
+
+            Square s(side);
+            Shape& shape = s;
+            cout << "Area = " << shape.area() << endl;
+            cout << "Perimeter = " << shape.perimeter() << endl;
+            break;
+        }
+
+        case 3: {
+            float length, breadth;
+            cout << "Enter length: ";
+            cin >> length;
+            cout << "Enter breadth: ";
+            cin >> breadth;
+
+            Rectangle r(length, breadth);
+            Shape& shape = r;
+            cout << "Area = " << shape.area() << endl;
+            cout << "Perimeter = " << shape.perimeter() << endl;
+            break;
+        }
+
+        default:
+            cout << "Invalid choice" << endl;
+    }
+
+}
